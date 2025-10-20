3fba2d59789f64a53f8a1498d94bf4272225c051
NP <narragufus210295@gmail.com>
2025-10-20 15:38:32 -0400

    First Commit for week1day1 assignments

---
+++ src/week1/day1/BasicArithmetic.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class BasicArithmetic {
+
+	public static void main(String[] args) {
+		int a = 42;
+		int b = 4;
+		
+		System.out.println("Addition: The Result is " + (a+b));
+		System.out.println("Subtraction : The Result is " + (a-b));
+		System.out.println("Multiply : The Result is " + (a*b));
+		System.out.println("Division : The Result is " + (a/b));
+		System.out.println("Reminder : The Result is " + (a%b));
+		
+	}
+
+}
---
+++ src/week1/day1/VariablesPractice.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class VariablesPractice {
+
+	public static void main(String[] args) {
+
+		String Name = "Prathyusha";
+		int Age = 27;
+		double Experience = 5.6;
+		
+		System.out.println("Name: " + Name);
+        System.out.println("Age: " + Age);
+        System.out.println("Experience: " + Experience + " Years");
+        
+
+	}
+
+}
